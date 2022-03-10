45a252c4e276056261cf4726f440bc3132b90af3
sbx_user1051 <sbx_user1051@169.254.212.197>
2022-03-10 09:13:03 +0000

    Thu Mar 10 09:13:03 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,11 +1,20 @@
-from lib.solutions.SUM.sum_solution import compute
+from lib.solutions.SUM.sum_solution import compute, NumberOutofRange
 import pytest
+
 
 def test_input_is_int():
     with pytest.raises(TypeError):
         compute('string', 0)
 
+def test_input_is_int2():
+    with pytest.raises(TypeError):
+        compute(5, 'String')
+
+def test_input_is_int3():
+    with pytest.raises(TypeError):
+        compute('String1', 'String2')
 
 def test_input_is_larger_than_100():
-    pass
+    with pytest.raises(NumberOutofRange):
+        compute(101, 4)
 
