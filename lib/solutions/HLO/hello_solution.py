041ad4eaba8c93e3d9e35ae29e2c39151b2bfdea
sbx_user1051 <sbx_user1051@169.254.212.197>
2022-03-10 09:25:56 +0000

    Thu Mar 10 09:25:56 UTC 2022

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,5 +2,19 @@
 
 # noinspection PyUnusedLocal
 # friend_name = unicode string
-def hello(friend_name):
-    raise NotImplementedError()
+
+"""
+In order to complete the round you need to implement the following method:
+     hello(String) -> String
+
+Where:
+ - param[0] = a String. Ignore for now.
+ - @return = a String containing a message
+
+"""
+
+def hello(friend_name: float = None) -> str:
+    return 'Hello World'
+
+if __name__ == '__main__':
+    print(hello())
