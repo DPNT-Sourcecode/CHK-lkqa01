e8896dfc332248259b7628abd70e7a81a97f948e
sbx_user1051 <sbx_user1051@169.254.212.197>
2022-03-10 09:01:03 +0000

    Thu Mar 10 09:01:03 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,12 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    raise NotImplementedError()
+# sum(Integer, Integer) -> Integer
+"""
+Where:
+ - param[0] = a positive integer between 0-100
+ - param[1] = a positive integer between 0-100
+ - @return = an Integer representing the sum of the two numbers
+"""
+
+def compute(x: int, y:int)-> int:
+    return x+y
+
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,7 @@
-from solutions.SUM import sum_solution
+from lib.solutions.SUM.sum_solution import compute
+import pytest
 
+def test_input_is_int():
+    with pytest.raises(TypeError):
+        compute('string', 0)
 
-class TestSum():
-    def test_sum(self):
-        assert sum_solution.compute(1, 2) == 3
