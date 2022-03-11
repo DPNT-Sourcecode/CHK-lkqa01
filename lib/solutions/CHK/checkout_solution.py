bccb75a8c3c21129109160539d7e676d2d2d6a0b
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 18:35:57 +0000

    Fri Mar 11 18:35:57 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,17 +2,36 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 
-available_products = ['A', 'B', 'C', 'D']
 
-prices = {'A': 50,
-          'B': 30,
-          'C': 20,
-          'D': 15}
+class Product:
+    def __init__(self, name: str, price: int, multi: list=[], BxGx: list=[]):
+        self.name = name
+        self.price = price
+        self.multi = multi
+        self.BxGx = BxGx
+
+    def __repr__(self):
+        return f'{self.name}'
 
 
-special_offers = {
-    'A': (3, 130),
-    'B': (2, 45),
+class Item:
+    def __init__(self, item: str, quantity: int):
+        self.item = product[item]
+        self.quantity = quantity
+
+    def __repr__(self):
+        return f'{self.quantity}{self.item}'
+
+    def pricing(self):
+        pass
+
+
+products = {
+    'A': Product('A', 50, multi=[(5, 200), (2, 120)]),
+    'B': Product('B', 30, multi=[(2, 45)]),
+    'C': Product('C', 20),
+    'D': Product('D', 15),
+    'E': Product('E', 40, BxGx=["CODED"]) #Come to this later.
 }
 
 
@@ -20,26 +39,18 @@ def checkout(skus: str) -> int:
     """Supermarket checkout function. it accepts a string of products as input and returns the total amount taking into
     account discounted items"""
     if not isinstance(skus, str):
-        raise ("Items should be a string")
+        raise "Items should be a string"
 
     for product in skus:
-        if product not in available_products:
+        if product not in product:
             return -1
 
-    items = {product: skus.count(product) for product in skus}
+    basket = [Item(i, skus.count(i)) for i in product]
+    print(basket)
 
-    pricing = [items[product] * prices[product] for product in items.keys() if product not in special_offers.keys()]
+    pricing = sum([i.pricing() for i in basket])
 
-    for product, value in zip(special_offers.keys(), special_offers.values()):
-        price = 0
-        if product in items:
-            if items[product] // value[0] >= 1:
-                price += (items[product] // value[0]) * value[1]
-
-            price += (items[product] % value[0]) * prices[product]
-            pricing.append(price)
-
-    return sum(pricing)
+    return pricing
 
 
 # sum2 = checkout('A')
