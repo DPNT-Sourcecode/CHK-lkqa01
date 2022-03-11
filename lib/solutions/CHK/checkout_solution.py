f49de9bc08b6d642e4ec6b8ac7742997480963c8
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 18:50:57 +0000

    Fri Mar 11 18:50:57 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -27,21 +27,48 @@ class Item:
         count = self.quantity
         offers = self.item.multi
 
-        for offer in sorted(offers, reverse=True): #Sorted to make sure we apply the highest offer first.
+        for offer in sorted(offers, reverse=True): # Sorted to make sure we apply the highest offer first.
             multiples = count // offer[0]
             if multiples >= 1:
                 price.append(multiples * offer[1])
                 count -= multiples * offer[0]
 
-    def pricing(self) -> int:
+        price.append(count * self.item.price)
+
+        return sum(price)
+
+    def buy_x_get_x(self, basket) -> int:
+        price = 0
+        offers = self.item.BxGx
+
+        for offer in offers:
+            offer = list(offer)
+            qualifying_num = int(offer[0])
+            free_item = products[offer[3]]
+            free_item_num = int(offer[2])
+
+        target = [x for x in basket if x.item.name == free_item.name and x.quantity > 0]
+
+        if target:
+            free_item = target[0]
+
+            if self.quantity >= qualifying_num and free_item.quantity > 0:
+                factor = self.quantity // qualifying_num
+                price = factor * free_item_num * free_item.item.price
+
+        return price
+
+    def pricing(self, basket) -> int:
         price = 0
         if self.item.multi:
-            pass
+            price += self.multi_pricing()
         else:
             price += self.item.price * self.quantity
 
         if self.item.BxGx:
-            pass
+            price -= self.buy_x_get_x(basket)
+
+        return price
 
 
 products = {
@@ -49,7 +76,7 @@ products = {
     'B': Product('B', 30, multi=[(2, 45)]),
     'C': Product('C', 20),
     'D': Product('D', 15),
-    'E': Product('E', 40, BxGx=["CODED"]) #Come to this later.
+    'E': Product('E', 40, BxGx=["2E1B"]) #f ormat: 2 of E gets 1 of B.
 }
 
 
@@ -63,13 +90,12 @@ def checkout(skus: str) -> int:
         if product not in product:
             return -1
 
-    basket = [Item(i, skus.count(i)) for i in product]
-    print(basket)
+    basket = [Item(i, skus.count(i)) for i in products]
 
-    pricing = sum([i.pricing() for i in basket])
+    pricing = sum([i.pricing(basket) for i in basket])
 
     return pricing
 
 
-# sum2 = checkout('A')
-# print(sum)
+total = checkout('AAA')
+print(total)
