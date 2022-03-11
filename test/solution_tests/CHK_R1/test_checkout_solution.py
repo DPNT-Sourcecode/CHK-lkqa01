a0c96f3e2fba59995a1f72ff45cdeef2c76b5254
sbx_user1051 <sbx_user1051@169.254.205.209>
2022-03-11 18:20:57 +0000

    Fri Mar 11 18:20:57 UTC 2022

--- test/solution_tests/CHK_R1/test_checkout_solution.py
+++ test/solution_tests/CHK_R1/test_checkout_solution.py
@@ -4,13 +4,15 @@ from lib.solutions.CHK.checkout_solution import checkout
 def test_correct_checkout_no_discount():
     assert checkout('AABCD') == 165
 
+
 def test_correct_checkout_with_discount():
     assert checkout('AAABBBCD') == 240
+
 
 def test_wrong_items():
     assert checkout('AAAEBBBCD') == -1
 
+
 def test_wrong_input():
     with pytest.raises(TypeError):
         checkout(10)
-
